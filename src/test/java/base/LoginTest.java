346358a86ec7086f479a535bf51bfaa2cab164c6
BinnyJ <binnyjohn39@gmail.com>
2020-02-26 16:40:44 -0600

    binny

--- src/test/java/base/LoginTest.java
+++ src/test/java/base/LoginTest.java
@@ -1,11 +1,11 @@
 package base;
 
-import org.testng.annotations.Test;
+
 import pages.LoginPage;
 
-public class LoginTest extends BaseTest {
+public class LoginTest  {
     LoginPage loginPage = new LoginPage();
-    @Test
+
     public void login(){
         loginPage.login();
     }
