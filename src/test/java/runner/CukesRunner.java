0c825473b6242d1227a0f5237e56ff4147eb96cc
venerakadyr <57569728+venerakadyr@users.noreply.github.com>
2020-02-26 21:19:20 -0600

    venera update

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -1,12 +1,16 @@
 package runner;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin="html:target/cucumber-reports",
+       plugin="json:target/Destination/cucumber.json",
         features = "src/test/resources/features",
         glue= "steps_definitions",
         dryRun =false,
-        tags="@smokeTest"
+        tags="@ind,@com"
 )
 
 public class CukesRunner {
