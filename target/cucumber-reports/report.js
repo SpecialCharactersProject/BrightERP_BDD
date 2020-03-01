840aaaad2e54c49b9c99074b3b004cc2ebd542d0
Mikail Durur <mikail.drr@gmail.com>
2020-02-29 18:43:47 -0600

    no base package

---
+++ target/cucumber-reports/report.js
@@ -0,0 +1,134 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/resources/features/import.feature");
+formatter.feature({
+  "name": "Import a File on Contacts Module",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@ImportFile"
+    }
+  ]
+});
+formatter.scenario({
+  "name": "Verifying the import button",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@ImportFile"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the contacts module",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_is_on_the_contacts_module()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user verify the import button displays",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_verify_the_import_button_displays()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the import button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_clicks_on_the_import_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user verify the current page title with the expected title",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_verify_the_current_page_title_with_the_expected_title()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": ": Validation of load file button",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@ImportFile"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user is on the contacts module",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_is_on_the_contacts_module()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user clicks on the import button",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_clicks_on_the_import_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user verify the Load File button displays",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_verify_the_Load_File_button_displays()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user verify the No file chosen… text on the load file input box",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_verify_the_No_file_chosen_text_on_the_load_file_input_box()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "user should pass the path of the file on",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Import_StepDefs.user_should_pass_the_path_of_the_file_on()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
---
+++ target/test-classes/features/import.feature
@@ -0,0 +1,16 @@
+@ImportFile
+Feature: Import a File on Contacts Module
+
+
+  Scenario: Verifying the import button
+    Given user is on the contacts module
+    And user verify the import button displays
+    And user clicks on the import button
+    Then user verify the current page title with the expected title
+
+  Scenario: : Validation of load file button
+    Given user is on the contacts module
+    When user clicks on the import button
+    And user verify the Load File button displays
+    And user verify the No file chosen… text on the load file input box
+    Then user should pass the path of the file on
