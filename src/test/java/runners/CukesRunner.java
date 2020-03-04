a0773a5307358ab0729fbf478009ded960e2c64b
milostela <milos.lekic88@outlook.com>
2020-03-03 18:45:59 -0600

    configuration.properties update

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -1,19 +1,19 @@
-package runners;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-
-        plugin={"json:target/destination/pretty-reports.json","html:target/cucumber-reports"},
-        features = "src/test/resources/features",
-        glue= "steps_definitions",
-        dryRun =false,
-        tags="@SC_SmokeTest"
-)
-
-public class CukesRunner {
-
-}
+//package runners;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//
+//        plugin={"json:target/destination/pretty-reports.json","html:target/cucumber-reports"},
+//        features = "src/test/resources/features",
+//        glue= "steps_definitions",
+//        dryRun =false,
+//        tags="@SC_SmokeTest"
+//)
+//
+//public class CukesRunner {
+//
+//}
